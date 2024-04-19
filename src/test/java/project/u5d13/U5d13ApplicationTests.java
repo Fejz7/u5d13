f969cf1d17bff0465873346ca61edbf6563d9193
Fejz7 <ZOAC@outlook.it>
2024-04-19 11:32:53 +0200

    changes

---
+++ src/test/java/project/u5d13/U5d13ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.u5d13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
